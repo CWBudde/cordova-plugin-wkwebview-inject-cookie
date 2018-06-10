c96e512b835b88d04ec618a69447cb4945bb695c
Christian <Christian@savioursofsoul.de>
2018-06-10 16:05:01 +0200

    Bump version

--- src/ios/WKWebViewCookieSync.m
+++ src/ios/WKWebViewCookieSync.m
@@ -1,3 +1,19 @@
+/* 
+ * Copyright 2018 Christian-W. Budde
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #import "WKWebViewCookieSync.h"
 #import <Cordova/CDV.h>
 
@@ -5,10 +21,12 @@
 
 - (void)sync:(CDVInvokedUrlCommand *)command {
   self.callbackId = command.callbackId;
-  NSLog(@"Test");
+  NSLog(@"Cookie sync attempt");
 
-  self.webView.configuration.processPool = [[WKProcessPool alloc] init];
+  WKWebView* wkWebView = (WKWebView*) self.webView;
+  wkWView.configuration.processPool = [[WKProcessPool alloc] init];
 
+	CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
 }
 
