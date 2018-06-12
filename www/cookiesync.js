aa7d53474fa46507ab0628b2795b9162791d3ece
Christian <Christian@savioursofsoul.de>
2018-06-12 16:53:56 +0200

    Typo

--- www/cookiesync.js
+++ www/cookiesync.js
@@ -21,6 +21,7 @@ module.exports = {
 
         if ((url.substr(0,4)=="http")&&(url.indexOf("\/\/")>=0)) {
           Delete(url,1,url.indexOf("\/\/")+2);
+        };
         var sPos = url.indexOf("\/");
         var domain = url.substr(0,sPos);
         var path = url.substr(sPos,(url.length-sPos));
